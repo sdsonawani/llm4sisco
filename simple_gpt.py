28337666a463b9f090189a85e20ea42a3c9156e8
Shubham Sonawani <shubham.sonawani@asu.edu>
2024-04-11 18:44:37 -0700

    initiation

---
+++ simple_gpt.py
@@ -0,0 +1,14 @@
+
+import os
+import sys
+import torch
+from torch import functional as F
+
+# ref: https://github.com/karpathy/nanoGPT/blob/master/model.py
+
+class SelfAttention(torch.nn):
+    def __init__(self):
+        super.()__init__()
+        pass
+
+
